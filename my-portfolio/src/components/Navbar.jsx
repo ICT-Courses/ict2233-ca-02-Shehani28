da58ca8a66fb65618f6ef7f2ba1584d2c45e17df
Shehani28 <ar112770@fhss.sjp.ac.lk>
2025-09-23 00:08:54 +0530

    Update navigation bar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ const Navbar = ({ darkMode, setDarkMode}) => {
   return (
     <nav className="fixed top-0 left-0 w-full p-2 flex justify-between items-center bg-white dark:bg-gray-800 z-50 shadow pl-8 text-white">
       <div className={StyleSheetList.brand}>
+         <div className="text-2xl font-bold text-gray-800 dark:text-white"></div>
         <NavLink to="/">Shehani</NavLink>
       </div>
 
-      <div className="flex item-center space-x-4">
+      <div className="hidden md:flex items-center space-x-6">
         <NavLink to="/" className={({isActive}) => `${styles.link} ${isActive ? styles.activeLink : ""}`}> Home </NavLink>
 
         <NavLink to="/about" className={({isActive}) => `${styles.link} ${isActive ? styles.activeLink : ""}`}>About</NavLink>
@@ -21,6 +22,9 @@ const Navbar = ({ darkMode, setDarkMode}) => {
           {darkMode ? "🌙 Dark" : "☀ Light"}
         </button>
       </div>
+      <div className="md:hidden">
+        </div>
+
     </nav>
   );
 };
