35291cc6ebac5e3c02d7e186adffdcf9acbdbda3
Shehani28 <ar112770@fhss.sjp.ac.lk>
2025-09-22 23:34:06 +0530

    Add About page with background information and skills

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -1,7 +1,49 @@
-import Navbar from "../components/Navbar";
+import React from "react";
+import styles from "../style/About.module.css";
+import profile from "../assets/Page.jpg";
 
 const About = () => {
-  return <div> About page</div>
+  return (
+              <section className={`${styles.contactContainer} w-full min-h-screen bg-blue-100 p-8`}>
+          <h2 className="text-3xl font-bold mb-8 mt-12 pl-35 text align-center ml-120">About Me</h2>
+
+        <p className="text-lg mb-6 leading-relaxed text-gray-800">
+          I am a second year undergraduate at
+         University of Sri Jayewardenepura
+          pursing a BSc(Hons) in Information Technology.
+          In addition, I am completing an English Diploma
+          to improve my communication skills.
+       </p>
+
+       <h2 className="text-2xl font-semibold mb-3 text-gray-900">Technical Skills</h2>
+       <ul className="list-disc list-inside mb-6 text-gray-800">
+        <li>Frontend: React, HTML, CSS, TailwindCSS, JavaScript</li>
+        <li>Backend basics: C#, Java, Node.js</li>
+        <li>Databases: SQL Server, MySQL</li>
+        <li>Tools: Git, GitHub, VS Code, Figma</li>
+       </ul>
+
+       <h2 className="text-2xl font-semibold mb-3 text-gray-900">Soft Skills</h2>
+       <ul className="list-disc list-inside mb-6 text-gray-800">
+        <li>Communication</li>
+        <li>Teamwork</li>
+        <li>Problem Solving</li>
+        <li>Time Management</li>
+        <li>Adaptability</li>
+       </ul>
+
+       <div className=" flex justify-center lg:justify-end mt-8 lg:mt-0">
+               <div className=" -mt-90 mr-30">
+                 <img 
+                 src={profile}
+                 alt="Shehani"
+                 className=" object-cover rounded-lg border-none w-80 h-80"
+                  />
+    </div>
+    </div>
+    </section>
+    
+  );
 };
 
 export default About;
